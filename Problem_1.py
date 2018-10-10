9c63fe4079e356eb15edf574b9e6b36f3fc8a40a
Marshall Pontrelli <37884270+mpontrelli@users.noreply.github.com>
2018-10-10 17:41:13 -0400

    made Changes

---
+++ Problem_1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 10 14:21:28 2018
+
+@author: mpontr01
+"""
+
+import numpy as np
+from scipy.stats import lognorm
+import matplotlib.pyplot as plt
+import seaborn as sb
+import pandas as pd
+
+O = np.zeros((100000, 12))
+
+#Sample sizes of 25
+for i in range(100000):
+    X = np.random.lognormal(0,1,25)
+    O[i,0] = np.sum(X)/len(X)
+    
+
+
+
+#df3 = pd.DataFrame(O,columns=['25X1', '25X2', '25x0.51', '25x0.52',
+           #        '50X1', '50X2', '50x0.51', '50x0.52',
+           #        '100X1', '100X2', '100x0.51', '100x0.52',])
+
+# Direct generation
+
+
+#I don't know the right way to code this...
+#U = np.random.normal(0,1,100000)
+#X = np.exp(U)
+#plt.figure()
+#sb.distplot(X,hist = True)
---
+++ Problem_2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 10 17:29:46 2018
+
+@author: mpontr01
+"""
+
+#Problem 2
