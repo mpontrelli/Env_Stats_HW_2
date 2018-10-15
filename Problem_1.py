1262332b7a836920acf9f279e3c0e68ce4fc7fe1
Marshall Pontrelli <37884270+mpontrelli@users.noreply.github.com>
2018-10-15 18:24:44 -0400

    add changes

--- Problem_1.py
+++ Problem_1.py
@@ -11,74 +11,216 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 import pandas as pd
 
+###############################################################################
+#Parts a and b
+uy = 0
+sy = 1.0
+true_mean = np.exp(uy+sy/2)
+true_median = np.exp(uy)
+nonparmeanbias = []
+nonparmeanvar = []
+nonparmeanMSE = []
+nonparmedianbias = []
+nonparmedianvar = []
+nonparmedianMSE = []
+parmeanbias = []
+parmeanMSE = []
+parmedianbias = []
+parmeanvar = []
+parmedianvar = []
+parmedianMSE = []
+X1mean = []
+X2mean = []
+X1med = []
+X2med = []
 meanmatrix = []
 dfmean = pd.DataFrame
 medianmatrix = []
 dfmedian = pd.DataFrame
-#O = np.zeros((100000,12))
 #Sample sizes of 25
 for i in range(100000):
-    X = np.random.lognormal(0,1,25)
-    X1 = np.sum(X)/len(X) #estimate of mean
-    #O[i, 0] = X1
+    X = np.random.lognormal(uy,sy,25)
+    X1 = np.mean(X)#estimate of mean
+    X1mean = np.append(X1mean,X1)
     meanmatrix.append({'mean': X1, 'P_non_P': 'Non Parametric', 'sample_size':'25' })
-    
     yi = np.log(X)
-    ybar = sum(yi) / len(yi)
-    vy = sum((yi-ybar)**2)/len(yi)
+    ybar = np.mean(yi)
+    vy = np.var(yi)
     X2 = np.exp(ybar + vy/2)
+    X2mean = np.append(X2mean,X2)
     meanmatrix.append({'mean': X2, 'P_non_P': 'Parametric', 'sample_size':'25' })
-    #O[i, 1] = X2
     b = np.sort(X) #sort to estimate median
     Median1 = b[int((len(b)+1)/2-1)]
+    X1med = np.append(X1med,Median1)
     medianmatrix.append({'median': Median1, 'P_non_P': 'Non Parametric', 'sample_size':'25' })
     Median2 = np.exp(ybar) #parametric estimate of median
+    X2med = np.append(X2med,Median2)
     medianmatrix.append({'median': Median2, 'P_non_P': 'Parametric', 'sample_size':'25' })
+    
+a = (np.mean(X1mean)-true_mean)**2
+nonparmeanbias = np.append(nonparmeanbias, a)
+a = np.var(X1mean)
+nonparmeanvar = np.append(nonparmeanvar, a)
+a = nonparmeanvar[0] +nonparmeanbias[0]
+nonparmeanMSE = np.append(nonparmeanMSE, a)
+
+
+a = (np.mean(X2mean)-true_mean)**2
+parmeanbias = np.append(parmeanbias, a)
+a = np.var(X2mean)
+parmeanvar = np.append(parmeanvar, a)
+a = parmeanvar[0] +parmeanbias[0]
+parmeanMSE = np.append(parmeanMSE, a)
+
+a = (np.mean(X1med)-true_median)**2
+nonparmedianbias = np.append(nonparmedianbias, a)
+a = np.var(X1med)
+nonparmedianvar = np.append(nonparmedianvar, a)
+a = nonparmedianvar[0] +nonparmedianbias[0]
+nonparmedianMSE = np.append(nonparmedianMSE, a)
+
+a = (np.mean(X2med)-true_median)**2
+parmedianbias = np.append(parmedianbias, a)
+a = np.var(X2med)
+parmedianvar = np.append(parmedianvar, a)
+a = parmedianvar[0] +parmedianbias[0]
+parmedianMSE = np.append(parmedianMSE, a)
+
+
+
+X1mean = []
+X2mean = []
+X1med = []
+X2med = []
 
 #Sample sizes of 50
 for i in range(100000):
-    X = np.random.lognormal(0,1,50)
-    X1 = np.sum(X)/len(X) #estimate of mean
-    #O[i, 0] = X1
+    X = np.random.lognormal(uy,sy,50)
+    X1 = np.mean(X) #estimate of mean
+    X1mean = np.append(X1mean,X1)
     meanmatrix.append({'mean': X1, 'P_non_P': 'Non Parametric', 'sample_size':'50' })
-    
     yi = np.log(X)
-    ybar = sum(yi) / len(yi)
-    vy = sum((yi-ybar)**2)/len(yi)
+    ybar = np.mean(yi)
+    vy = np.var(yi)
     X2 = np.exp(ybar + vy/2)
+    X2mean = np.append(X2mean,X2)
     meanmatrix.append({'mean': X2, 'P_non_P': 'Parametric', 'sample_size':'50' })
-    #O[i, 1] = X2
     b = np.sort(X) #sort to estimate median
     Median1 = (b[len(b)/2-1]+b[len(b)/2])/2
+    X1med = np.append(X1med,Median1)
     medianmatrix.append({'median': Median1, 'P_non_P': 'Non Parametric', 'sample_size':'50' })
     Median2 = np.exp(ybar) #parametric estimate of median
+    X2med = np.append(X2med,Median2)
     medianmatrix.append({'median': Median2, 'P_non_P': 'Parametric', 'sample_size':'50' })
-        
+
+a = (np.mean(X1mean)-true_mean)**2
+nonparmeanbias = np.append(nonparmeanbias, a)
+a = np.var(X1mean)
+nonparmeanvar = np.append(nonparmeanvar, a)
+a = nonparmeanvar[1] +nonparmeanbias[1]
+nonparmeanMSE = np.append(nonparmeanMSE, a)
+
+
+a = (np.mean(X2mean)-true_mean)**2
+parmeanbias = np.append(parmeanbias, a)
+a = np.var(X2mean)
+parmeanvar = np.append(parmeanvar, a)
+a = parmeanvar[1] +parmeanbias[1]
+parmeanMSE = np.append(parmeanMSE, a)
+
+a = (np.mean(X1med)-true_median)**2
+nonparmedianbias = np.append(nonparmedianbias, a)
+a = np.var(X1med)
+nonparmedianvar = np.append(nonparmedianvar, a)
+a = nonparmedianvar[1] +nonparmedianbias[1]
+nonparmedianMSE = np.append(nonparmedianMSE, a)
+
+a = (np.mean(X2med)-true_median)**2
+parmedianbias = np.append(parmedianbias, a)
+a = np.var(X2med)
+parmedianvar = np.append(parmedianvar, a)
+a = parmedianvar[1] +parmedianbias[1]
+parmedianMSE = np.append(parmedianMSE, a)
+
+
+
+X1mean = []
+X2mean = []
+X1med = []
+X2med = []
+ 
  
 
 #Sample sizes of 100
 for i in range(100000):
-    X = np.random.lognormal(0,1,100)
-    X1 = np.sum(X)/len(X) #estimate of mean
-    #O[i, 0] = X1
+    X = np.random.lognormal(uy,sy,100)
+    X1 = np.mean(X) #estimate of mean
+    X1mean = np.append(X1mean,X1)
     meanmatrix.append({'mean': X1, 'P_non_P': 'Non Parametric', 'sample_size':'100' })
-    
     yi = np.log(X)
-    ybar = sum(yi) / len(yi)
-    vy = sum((yi-ybar)**2)/len(yi)
+    ybar = np.mean(yi)
+    vy = np.var(yi)
     X2 = np.exp(ybar + vy/2)
+    X2mean = np.append(X2mean,X2)
     meanmatrix.append({'mean': X2, 'P_non_P': 'Parametric', 'sample_size':'100' })
-    #O[i, 1] = X2
     b = np.sort(X) #sort to estimate median
     Median1 = (b[len(b)/2-1]+b[len(b)/2])/2
+    X1med = np.append(X1med,Median1)
     medianmatrix.append({'median': Median1, 'P_non_P': 'Non Parametric', 'sample_size':'100' })
     Median2 = np.exp(ybar) #parametric estimate of median
+    X2med = np.append(X2med,Median2)
     medianmatrix.append({'median': Median2, 'P_non_P': 'Parametric', 'sample_size':'100' })
-       
+
+a = (np.mean(X1mean)-true_mean)**2
+nonparmeanbias = np.append(nonparmeanbias, a)
+a = np.var(X1mean)
+nonparmeanvar = np.append(nonparmeanvar, a)
+a = nonparmeanvar[2] +nonparmeanbias[2]
+nonparmeanMSE = np.append(nonparmeanMSE, a)
+
+
+a = (np.mean(X2mean)-true_mean)**2
+parmeanbias = np.append(parmeanbias, a)
+a = np.var(X2mean)
+parmeanvar = np.append(parmeanvar, a)
+a = parmeanvar[2] +parmeanbias[2]
+parmeanMSE = np.append(parmeanMSE, a)
+
+a = (np.mean(X1med)-true_median)**2
+nonparmedianbias = np.append(nonparmedianbias, a)
+a = np.var(X1med)
+nonparmedianvar = np.append(nonparmedianvar, a)
+a = nonparmedianvar[2] +nonparmedianbias[2]
+nonparmedianMSE = np.append(nonparmedianMSE, a)
+
+a = (np.mean(X2med)-true_median)**2
+parmedianbias = np.append(parmedianbias, a)
+a = np.var(X2med)
+parmedianvar = np.append(parmedianvar, a)
+a = parmedianvar[2] +parmedianbias[2]
+parmedianMSE = np.append(parmedianMSE, a)
+
+
+
+X1mean = []
+X2mean = []
+X1med = []
+X2med = []
+
+    
 dfmean = pd.DataFrame(meanmatrix)    
 dfmedian = pd.DataFrame(medianmatrix)
 
 plt.figure()
 ax = sb.boxplot(x = "sample_size", y = "mean", hue = "P_non_P", data=dfmean, palette = "Set3", order = ["25", "50", "100"])
+plt.title('Mean')
+plt.savefig('mean_boxplot.pdf')  
 plt.figure()
 ax2 = sb.boxplot(x = "sample_size", y = "median", hue = "P_non_P", data=dfmedian, palette = "Set3", order = ["25", "50", "100"])
+plt.title('Median')
+plt.savefig('median_boxplot.pdf')  
+
+plt.plot(XX, nonparmeanbias, color = 'r', linestyle = '--')
+plt.show()
+###############################################################################
+#Part c
