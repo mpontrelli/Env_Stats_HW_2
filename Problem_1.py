28795873382857158a38685922bfbad8ded17613
Marshall Pontrelli <37884270+mpontrelli@users.noreply.github.com>
2018-10-15 09:41:15 -0400

    made changes

--- Problem_1.py
+++ Problem_1.py
@@ -11,91 +11,74 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 import pandas as pd
 
-O = np.zeros((100000, 12))
-
+meanmatrix = []
+dfmean = pd.DataFrame
+medianmatrix = []
+dfmedian = pd.DataFrame
+#O = np.zeros((100000,12))
 #Sample sizes of 25
 for i in range(100000):
     X = np.random.lognormal(0,1,25)
     X1 = np.sum(X)/len(X) #estimate of mean
-    O[i, 0] = X1
-    
+    #O[i, 0] = X1
+    meanmatrix.append({'mean': X1, 'P_non_P': 'Non Parametric', 'sample_size':'25' })
     
     yi = np.log(X)
     ybar = sum(yi) / len(yi)
     vy = sum((yi-ybar)**2)/len(yi)
     X2 = np.exp(ybar + vy/2)
-    O[i, 1] = X2
-    
-    O[i,3] = np.exp(X1) #parametric estimate of median
+    meanmatrix.append({'mean': X2, 'P_non_P': 'Parametric', 'sample_size':'25' })
+    #O[i, 1] = X2
     b = np.sort(X) #sort to estimate median
-    if len(X) % 2 == 0: #if n is even
-        O[i,2] = (b[len(b)/2-1]+b[len(b)/2])//2
-    else: #if n is odd
-        O[i,2] = b[int((len(b)+1)/2-1)]
-        
-        
+    Median1 = b[int((len(b)+1)/2-1)]
+    medianmatrix.append({'median': Median1, 'P_non_P': 'Non Parametric', 'sample_size':'25' })
+    Median2 = np.exp(ybar) #parametric estimate of median
+    medianmatrix.append({'median': Median2, 'P_non_P': 'Parametric', 'sample_size':'25' })
+
 #Sample sizes of 50
 for i in range(100000):
-    vy = np.array([])
     X = np.random.lognormal(0,1,50)
     X1 = np.sum(X)/len(X) #estimate of mean
-    O[i, 4] = X1
+    #O[i, 0] = X1
+    meanmatrix.append({'mean': X1, 'P_non_P': 'Non Parametric', 'sample_size':'50' })
     
-    for j in range(len(X)):
-        q = (np.log(X[j]) - X1)**2
-        vy = np.append(vy, q)
-    z = sum(vy)/len(X)
-    X2 = np.exp(X1 + z/2)
-    O[i, 5] = X2
-    
-    O[i,7] = np.exp(X1) #parametric estimate of median
+    yi = np.log(X)
+    ybar = sum(yi) / len(yi)
+    vy = sum((yi-ybar)**2)/len(yi)
+    X2 = np.exp(ybar + vy/2)
+    meanmatrix.append({'mean': X2, 'P_non_P': 'Parametric', 'sample_size':'50' })
+    #O[i, 1] = X2
     b = np.sort(X) #sort to estimate median
-    if len(X) % 2 == 0: #if n is even
-        O[i,6] = (b[len(b)//2-1]+b[len(b)//2])//2
-    else: #if n is odd
-        O[i,6] = b[(len(b)+1)//2-1]
+    Median1 = (b[len(b)/2-1]+b[len(b)/2])/2
+    medianmatrix.append({'median': Median1, 'P_non_P': 'Non Parametric', 'sample_size':'50' })
+    Median2 = np.exp(ybar) #parametric estimate of median
+    medianmatrix.append({'median': Median2, 'P_non_P': 'Parametric', 'sample_size':'50' })
         
-        
-        
+ 
+
 #Sample sizes of 100
 for i in range(100000):
-    vy = np.array([])
     X = np.random.lognormal(0,1,100)
     X1 = np.sum(X)/len(X) #estimate of mean
-    O[i, 8] = X1
-    for j in range(len(X)):
-        q = (np.log(X[j]) - X1)**2
-        vy = np.append(vy, q)
-    z = sum(vy)/len(X)
-    X2 = np.exp(X1 + z/2)
-    O[i, 9] = X2
-    O[i,11] = np.exp(X1) #parametric estimate of median
-    b = np.sort(X) #sort to estimate median
-    if len(X) % 2 == 0: #if n is even
-        O[i,10] = (b[len(b)//2-1]+b[len(b)//2])//2
-    else: #if n is odd
-        O[i,10] = b[(len(b)+1)//2-1]
-      
-        
-            
-        
+    #O[i, 0] = X1
+    meanmatrix.append({'mean': X1, 'P_non_P': 'Non Parametric', 'sample_size':'100' })
     
-#    
-#
-##Testing github
-## another test
-#
-#df3 = pd.DataFrame(O,columns=['25X1', '25X2', '25x0.51', '25x0.52',
-#                   '50X1', '50X2', '50x0.51', '50x0.52',
-#                   '100X1', '100X2', '100x0.51', '100x0.52',])
+    yi = np.log(X)
+    ybar = sum(yi) / len(yi)
+    vy = sum((yi-ybar)**2)/len(yi)
+    X2 = np.exp(ybar + vy/2)
+    meanmatrix.append({'mean': X2, 'P_non_P': 'Parametric', 'sample_size':'100' })
+    #O[i, 1] = X2
+    b = np.sort(X) #sort to estimate median
+    Median1 = (b[len(b)/2-1]+b[len(b)/2])/2
+    medianmatrix.append({'median': Median1, 'P_non_P': 'Non Parametric', 'sample_size':'100' })
+    Median2 = np.exp(ybar) #parametric estimate of median
+    medianmatrix.append({'median': Median2, 'P_non_P': 'Parametric', 'sample_size':'100' })
+       
+dfmean = pd.DataFrame(meanmatrix)    
+dfmedian = pd.DataFrame(medianmatrix)
 
-
-
-# Direct generation
-
-
-#I don't know the right way to code this...
-#U = np.random.normal(0,1,100000)
-#X = np.exp(U)
-#plt.figure()
-#sb.distplot(X,hist = True)
+plt.figure()
+ax = sb.boxplot(x = "sample_size", y = "mean", hue = "P_non_P", data=dfmean, palette = "Set3", order = ["25", "50", "100"])
+plt.figure()
+ax2 = sb.boxplot(x = "sample_size", y = "median", hue = "P_non_P", data=dfmedian, palette = "Set3", order = ["25", "50", "100"])
